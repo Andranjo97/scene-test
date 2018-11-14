92b1d1f710d01d5382afb4d5c07083ecb7425602
jnaranjo <jnaranjo@ioet.com>
2018-11-14 11:26:54 -0500

    change light color

--- src/components/threejs/GeneralLights.js
+++ src/components/threejs/GeneralLights.js
@@ -1,12 +1,12 @@
 import {PointLight} from "three";
 
 export default scene => {
-    const light = new PointLight("#ffffff", 1);
+    const light = new PointLight("#fff", 1);
     scene.add(light);
 
     const update = (time) => {
         light.intensity = (Math.sin(time)+1.5)/1.5;
-        light.color.setHSL(Math.sin(time), 1, 0.5);
+        //light.color.setHSL(Math.sin(time), 0.5, 0.5);
     };
 
     return {
